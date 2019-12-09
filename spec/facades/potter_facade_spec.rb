64bac2854729389dded3c3362718b73bdd3fc088
Michael Cooper <macoop2363@gmail.com>
2019-12-09 11:04:50 -0700

    Add test for PotterFacade#house_name

--- spec/facades/potter_facade_spec.rb
+++ spec/facades/potter_facade_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe PotterFacade do
     @facade = PotterFacade.new('Gryffindor')
   end
 
+  it '#house_name', :vcr do
+    expect(@facade.house_name).to eq('Gryffindor')
+  end
+
   it '#house_members_count', :vcr do
     expect(@facade.house_members_count).to eq(21)
   end
