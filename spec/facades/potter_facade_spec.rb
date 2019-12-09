7bb2e3fd827a5131e25b9164c40cd064048bb5f0
Michael Cooper <macoop2363@gmail.com>
2019-12-09 10:54:59 -0700

    Add test for PotterFacade to be used in the search controller

---
+++ spec/facades/potter_facade_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe PotterFacade do
+  before :each do
+    @facade = PotterFacade.new('Gryffindor')
+  end
+
+  it '#house_members_count', :vcr do
+    expect(@facade.house_members_count).to eq(21)
+  end
+end
